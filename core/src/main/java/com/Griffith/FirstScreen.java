94bc72eb36f68ece72f60fe8e16126828ebc79e7
egegucveren <123471466+egegucveren@users.noreply.github.com>
2026-03-23 18:12:17 +0000

    Added basic gameplay prototype with movement, jump, collision, and simple game rules

--- core/src/main/java/com/Griffith/FirstScreen.java
+++ core/src/main/java/com/Griffith/FirstScreen.java
@@ -1,43 +1,142 @@
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
-/** First screen of the application. Displayed after the application is created. */
 public class FirstScreen implements Screen {
+
+    private ShapeRenderer shapeRenderer;
+
+    // Fireboy
+    float fireX = 100, fireY = 100;
+    float fireVelocityY = 0;
+
+    // Watergirl
+    float waterX = 200, waterY = 100;
+    float waterVelocityY = 0;
+
+    float speed = 200;
+    float gravity = -500;
+    float jumpPower = 300;
+
+    float groundY = 50;
+
+    // Hazard (water pool)
+    float hazardX = 300;
+    float hazardWidth = 100;
+
+      // Goal (door)
+    float doorX = 550;
+
     @Override
     public void show() {
-        // Prepare your screen here.
+        shapeRenderer = new ShapeRenderer();
     }
 
     @Override
     public void render(float delta) {
-        // Draw your screen here. "delta" is the time since last render in seconds.
+
+        // Clear screen
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // INPUTS FOR MOVEMENT 
+
+        // Fireboy (A, D)
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) fireX -= speed * delta;
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) fireX += speed * delta;
+
+        // Watergirl (LEFT, RIGHT)
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) waterX -= speed * delta;
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) waterX += speed * delta;
+
+        // JUMP
+
+        // Fireboy jump (W)
+        if (Gdx.input.isKeyJustPressed(Input.Keys.W) && fireY <= groundY) {
+            fireVelocityY = jumpPower;
+        }
+        // Watergirl jump (UP)
+        if (Gdx.input.isKeyJustPressed(Input.Keys.UP) && waterY <= groundY) {
+            waterVelocityY = jumpPower;
+        }
+
+        // GRAVITY
+
+        fireVelocityY += gravity * delta;
+        fireY += fireVelocityY * delta;
+
+        waterVelocityY += gravity * delta;
+        waterY += waterVelocityY * delta;
+
+        // GROUND COLLISION
+
+        if (fireY < groundY) {
+            fireY = groundY;
+            fireVelocityY = 0;
+        }
+
+        if (waterY < groundY) {
+            waterY = groundY;
+            waterVelocityY = 0;
+        }
+
+        // HAZARD CHECK
+
+        if (fireX > hazardX && fireX < hazardX + hazardWidth && fireY <= groundY) {
+            System.out.println("Fireboy died in water!");
+        }
+
+        // GOAL CHECK
+
+        if (fireX > doorX && waterX > doorX) {
+            System.out.println("Level Complete!");
+        }
+
+        // DRAW
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+        // Ground
+        shapeRenderer.setColor(0.5f, 0.5f, 0.5f, 1);
+        shapeRenderer.rect(0, 0, 800, groundY);
+
+        // Fireboy (red)
+        shapeRenderer.setColor(1, 0, 0, 1);
+        shapeRenderer.rect(fireX, fireY, 30, 30);
+
+        // Watergirl (blue)
+        shapeRenderer.setColor(0, 0, 1, 1);
+        shapeRenderer.rect(waterX, waterY, 30, 30);
+
+        // Hazard (cyan)
+        shapeRenderer.setColor(0, 1, 1, 1);
+        shapeRenderer.rect(hazardX, groundY, hazardWidth, 30);
+
+        // Door (green)
+        shapeRenderer.setColor(0, 1, 0, 1);
+        shapeRenderer.rect(doorX, groundY, 30, 60);
+
+        shapeRenderer.end();
     }
 
     @Override
     public void resize(int width, int height) {
-        // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
-        // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
-        if(width <= 0 || height <= 0) return;
-
-        // Resize your screen here. The parameters represent the new window size.
+        if (width <= 0 || height <= 0) return;
     }
 
     @Override
-    public void pause() {
-        // Invoked when your application is paused.
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-        // Invoked when your application is resumed after pause.
-    }
+    public void resume() {}
 
     @Override
-    public void hide() {
-        // This method is called when another screen replaces this one.
-    }
+    public void hide() {}
 
     @Override
     public void dispose() {
-        // Destroy screen's assets here.
+        shapeRenderer.dispose();
     }
 }
